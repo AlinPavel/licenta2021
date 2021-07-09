05412bbd1b440a8d48c36d5a432da9b91af5a88b
Alin98 <38643372+Alin98@users.noreply.github.com>
2021-07-09 17:43:43 +0300

    Initial version of Licenta2021 generated by JHipster-7.0.1

---
+++ src/test/java/com/licenta/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.licenta;
+
+import com.licenta.Licenta2021App;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = Licenta2021App.class)
+public @interface IntegrationTest {
+}
